34d4be5d17bc3290b997759a91955b09e021a871
Jackie <jzhou39@gmail.com>
2021-10-12 17:08:58 -0400

    changes

--- Assets/Scripts/ClickAudioPlayer.cs
+++ Assets/Scripts/ClickAudioPlayer.cs
@@ -1,24 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickAudioPlayer : MonoBehaviour
 {
     public AudioSource _audiosource;
 
-    private void Awake() {
+    private void Awake()
+    {
         _audiosource = GetComponent<AudioSource>();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         ClickHealth.clicked += PlayAudio;
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         ClickHealth.clicked -= PlayAudio;
     }
 
-    private void PlayAudio(){
+    private void PlayAudio()
+    {
         _audiosource.Play();
     }
 }
